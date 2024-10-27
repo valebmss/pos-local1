de0905a8c63855ab94b6c2af30c7f78d86e9fa16
valebmss <leidy.larrea01@usa.edu.co>
2024-10-27 12:05:33 -0500

    no fact

--- app/factura/page.js
+++ app/factura/page.js
@@ -1,28 +1,8 @@
 'use client'; // Indica que este componente es un Client Component
 
-import { useSearchParams } from 'next/navigation'; // Importar useSearchParams
 import { useEffect, useState } from 'react'; // Importar useEffect y useState
 
 export default function Factura() {
-  const searchParams = useSearchParams(); // Obtener los parámetros de búsqueda
-  const id = searchParams.get('id'); // Obtener el ID de la venta
-  const [factura, setFactura] = useState(null); // Estado para almacenar la factura
-
-  useEffect(() => {
-    const obtenerFactura = async () => {
-      if (id) {
-        try {
-          const response = await fetch(`/api/factura?id=${id}`); // Ajusta esta URL a tu API
-          const data = await response.json();
-          setFactura(data);
-        } catch (error) {
-          console.error("Error al obtener la factura:", error);
-        }
-      }
-    };
-
-    obtenerFactura(); // Llamar a la función para obtener la factura
-  }, [id]); // Ejecutar el efecto cada vez que cambie el ID
 
   return (
     <div>
